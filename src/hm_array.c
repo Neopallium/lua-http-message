ba2aabd8386691638d681636da2852b462fb9c3f
Robert G. Jakabosky <bobby@sharedrealm.com>
2013-05-19 22:42:03 -0700

    Add 'count' field to HMArray.

--- src/hm_array.c
+++ src/hm_array.c
@@ -10,18 +10,22 @@
 void *hm_array_resize_internal(void *ary_p, size_t elem_size, size_t capacity) {
 	HMArray *ary = NULL;
 
-	if(ary_p) {
-		ary = hm_array_from_void(ary_p);
-	}
 	/* if new capacity is zero just free the array. */
 	if(capacity == 0) {
-		free(ary);
+		hm_array_free_internal(ary_p);
 		return NULL;
+	}
+	if(ary_p) {
+		ary = hm_array_from_void(ary_p);
 	}
 
 	/* resize old array or allocate new array. */
 	ary = (HMArray *)realloc(ary, (sizeof(HMArray) + (elem_size * capacity)));
 	if(ary != NULL) {
+		if(ary_p == NULL) {
+			/* new array. */
+			ary->count = 0;
+		}
 		ary->capacity = capacity;
 		return hm_array_to_void(ary);
 	}
@@ -29,11 +33,12 @@ void *hm_array_resize_internal(void *ary_p, size_t elem_size, size_t capacity) {
 	return NULL;
 }
 
-void hm_array_free(void *ary_p) {
+void hm_array_free_internal(void *ary_p) {
 	HMArray *ary;
 	if(ary_p) {
 		ary = hm_array_from_void(ary_p);
 		ary->capacity = 0xDEAD;
+		ary->count = 0;
 		free(ary);
 	}
 }
--- src/hm_array.h
+++ src/hm_array.h
@@ -10,6 +10,7 @@
 typedef struct HMArray HMArray;
 
 struct HMArray {
+	uint16_t count;
 	uint16_t capacity;
 } L_GNUC_PACKED;
 
@@ -19,15 +20,25 @@ struct HMArray {
 
 L_LIB_API void *hm_array_resize_internal(void *ary_p, size_t elem_size, size_t capacity);
 
+L_LIB_API void hm_array_free_internal(void *ary_p);
+
 #define hm_array_capacity(ary_p) \
 	(hm_array_from_void(ary_p)->capacity)
 
-#define hm_array_resize(_type, ary_p, capacity) \
-	(_type *)hm_array_resize_internal(ary_p, sizeof(_type), capacity)
+#define hm_array_count(ary_p) \
+	(hm_array_from_void(ary_p)->count)
 
-#define hm_array_new(_type, capacity) \
-	(_type *)hm_array_resize_internal(NULL, sizeof(_type), capacity)
+#define hm_array_set_count(ary_p, _count) \
+	(hm_array_from_void(ary_p)->count) = (_count)
 
-L_LIB_API void hm_array_free(void *ary_p);
+#define hm_array_resize(ary_p, capacity) \
+	(ary_p) = (typeof(ary_p)) hm_array_resize_internal(ary_p, sizeof(typeof(ary_p[0])), capacity)
+
+#define hm_array_new(ary_p, capacity) \
+	(ary_p) = (typeof(ary_p)) hm_array_resize_internal(NULL, sizeof(typeof(ary_p[0])), capacity)
+
+#define hm_array_free(ary_p) do { \
+	hm_array_free_internal(ary_p); (ary_p) = NULL; \
+} while(0)
 
 #endif /* __HM_ARRAY_H__ */
